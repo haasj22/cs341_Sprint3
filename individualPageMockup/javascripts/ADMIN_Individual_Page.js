0fdef89d3006ba4007b1c62d234bab087efc5c40
Daniel Co <43159113+co22@users.noreply.github.com>
2021-03-04 21:26:21 -0800

    Added ability for user to upload image files using the edit equipment photo buttons
    -Added separate forms for editing each photo
    -Modified javascript code for opening/closing the forms
    -Added icons for edit photo buttons

--- individualPageMockup/javascripts/ADMIN_Individual_Page.js
+++ individualPageMockup/javascripts/ADMIN_Individual_Page.js
@@ -2,18 +2,35 @@
 
 $(document).ready(function()
 {
-    // openForm: opens the edit equipment photo form when edit button is clicked
+    // openForm: opens the edit main equipment photo form when edit button is clicked
     function openForm(event) {
         document.getElementById("editMainPhotoForm").style.display = "block";
     }
+
+    // openSubForm: opens the specified form for editing sub-photos when a button is clicked
+    function openSubForm1(event) {
+        document.getElementById("editSubPhotoForm1").style.display = "block";
+    }
+    function openSubForm2(event) {
+        document.getElementById("editSubPhotoForm2").style.display = "block";
+    }
+    function openSubForm3(event) {
+        document.getElementById("editSubPhotoForm3").style.display = "block";
+    }
   
-    // closeForm: closes the edit equipment photo form when close button is clicked
+    // closeForm: closes any open forms for editing equipment photos
     function closeForm(event) {
         document.getElementById("editMainPhotoForm").style.display = "none";
+        document.getElementById("editSubPhotoForm1").style.display = "none";
+        document.getElementById("editSubPhotoForm2").style.display = "none";
+        document.getElementById("editSubPhotoForm3").style.display = "none";
     }
 
     // call functions when buttons are clicked
-
     $(".edit_main_photo_button").on("click",openForm);
+    $(".edit_sub_photo_button1").on("click",openSubForm1);
+    $(".edit_sub_photo_button2").on("click",openSubForm2);
+    $(".edit_sub_photo_button3").on("click",openSubForm3);
     $(".close_edit_main_photo_button").on("click", closeForm);
+    $(".close_edit_sub_photo_button").on("click", closeForm);
 });
