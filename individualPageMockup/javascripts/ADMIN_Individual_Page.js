cb6fcf1a38234c3791225ec7a69dde69c966073f
Daniel Co <43159113+co22@users.noreply.github.com>
2021-03-16 11:22:19 -0700

    -added comments in admin individual js file

--- individualPageMockup/javascripts/ADMIN_Individual_Page.js
+++ individualPageMockup/javascripts/ADMIN_Individual_Page.js
@@ -28,6 +28,7 @@ $(document).ready(function()
 
     // changeImage: changes the main catalogue page image to the user uploaded image
     function changeMainImage(input) {
+    // Resource: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/4459419#4459419
         if (input.files && input.files[0]) {
             var reader = new FileReader();
             reader.onload = function(e) {
@@ -44,6 +45,7 @@ $(document).ready(function()
         changeMainImage(this)
     });
 
+    // changeSubImage1: changes the 1st sub image to the user uploaded image
     function changeSubImage1(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
@@ -61,6 +63,7 @@ $(document).ready(function()
         changeSubImage1(this)
     });
 
+    // changeSubImage2: changes the 2nd sub image to the user uploaded image
     function changeSubImage2(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
@@ -78,6 +81,7 @@ $(document).ready(function()
         changeSubImage2(this)
     });
 
+    // changeSubImage3: changes the 3rd sub image to the user uploaded image
     function changeSubImage3(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
