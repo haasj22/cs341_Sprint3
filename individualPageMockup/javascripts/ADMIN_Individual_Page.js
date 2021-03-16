c0dd4b764702752c4a8037207df1e8c7377c840b
Daniel Co <43159113+co22@users.noreply.github.com>
2021-03-15 18:01:02 -0700

    images now change to filename
    -added save button css for sub-photos
    - changed image upload form button margins
    -images now change to the uploaded image's file name
    -ISSUE: browsers read "fakepath" instead of getting the actual path of the image

--- individualPageMockup/javascripts/ADMIN_Individual_Page.js
+++ individualPageMockup/javascripts/ADMIN_Individual_Page.js
@@ -26,6 +26,28 @@ $(document).ready(function()
         document.getElementById("editSubPhotoForm3").style.display = "none";
     }
 
+    // changeImage: changes the main catalogue page image to the user uploaded image
+    // ISSUE: "fakepath" is sent to browser rather than actual file path
+    function changeMainImage(event) {
+        var new_image = document.getElementById("upload_image_button_main").value;
+        $(".main_image").attr('src', new_image);
+    }
+
+    function changeSubImage1(event) {
+        var new_image = document.getElementById("upload_image_button1").value;
+        $(".sub-img-style1").attr('src', new_image);
+    }
+
+    function changeSubImage2(event) {
+        var new_image = document.getElementById("upload_image_button2").value;
+        $(".sub-img-style2").attr('src', new_image);
+    }
+
+    function changeSubImage3(event) {
+        var new_image = document.getElementById("upload_image_button3").value;
+        $(".sub-img-style3").attr('src', new_image);
+    }
+
     // call functions when buttons are clicked
     $(".edit_main_photo_button").on("click",openForm);
     $(".edit_sub_photo_button1").on("click",openSubForm1);
@@ -33,4 +55,8 @@ $(document).ready(function()
     $(".edit_sub_photo_button3").on("click",openSubForm3);
     $(".close_edit_main_photo_button").on("click", closeForm);
     $(".close_edit_sub_photo_button").on("click", closeForm);
+    $(".save_photo_button").on("click", changeMainImage);
+    $(".save_photo_button1").on("click", changeSubImage1);
+    $(".save_photo_button2").on("click", changeSubImage2);
+    $(".save_photo_button3").on("click", changeSubImage3);
 });
