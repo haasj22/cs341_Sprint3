0b71192de672bd6a577759648038a59a18a671e5
Daniel Co <43159113+co22@users.noreply.github.com>
2021-03-16 11:40:54 -0700

    removed forms from .js file
    -removed code for editing photo forms from admin individual js file (functions and onclick events)

--- individualPageMockup/javascripts/ADMIN_Individual_Page.js
+++ individualPageMockup/javascripts/ADMIN_Individual_Page.js
@@ -2,30 +2,6 @@
 
 $(document).ready(function()
 {
-    // openForm: opens the edit main equipment photo form when edit button is clicked
-    function openForm(event) {
-        document.getElementById("editMainPhotoForm").style.display = "block";
-    }
-
-    // openSubForm: opens the specified form for editing sub-photos when a button is clicked
-    function openSubForm1(event) {
-        document.getElementById("editSubPhotoForm1").style.display = "block";
-    }
-    function openSubForm2(event) {
-        document.getElementById("editSubPhotoForm2").style.display = "block";
-    }
-    function openSubForm3(event) {
-        document.getElementById("editSubPhotoForm3").style.display = "block";
-    }
-  
-    // closeForm: closes any open forms for editing equipment photos
-    function closeForm(event) {
-        document.getElementById("editMainPhotoForm").style.display = "none";
-        document.getElementById("editSubPhotoForm1").style.display = "none";
-        document.getElementById("editSubPhotoForm2").style.display = "none";
-        document.getElementById("editSubPhotoForm3").style.display = "none";
-    }
-
     // changeImage: changes the main catalogue page image to the user uploaded image
     function changeMainImage(input) {
     // Resource: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/4459419#4459419
@@ -98,12 +74,4 @@ $(document).ready(function()
         $("#edit_sub_photo_button3").change(function() {
         changeSubImage3(this)
     });
-
-    // call functions when buttons are clicked
-    $(".edit_main_photo_button").on("click",openForm);
-    $(".edit_sub_photo_button1").on("click",openSubForm1);
-    $(".edit_sub_photo_button2").on("click",openSubForm2);
-    $(".edit_sub_photo_button3").on("click",openSubForm3);
-    $(".close_edit_main_photo_button").on("click", closeForm);
-    $(".close_edit_sub_photo_button").on("click", closeForm);
 });
