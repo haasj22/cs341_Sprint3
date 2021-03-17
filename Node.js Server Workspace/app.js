945661aa0e85fb9848b9264bf18b34d16bdbd825
ajunkins <57961583+ajunkins@users.noreply.github.com>
2021-03-16 20:27:26 -0700

    Incorporated new HTML/CSS/JS files into workspace
    Moved the updated code from other groups into the node.js workspace. All frontend code including code from the HTML/CSS teams is in the public folder.

--- Node.js Server Workspace/app.js
+++ Node.js Server Workspace/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testRouter = require('./routes/test');
 var catalogDataRouter = require('./routes/catalogData');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/test', testRouter);
 app.use('/catalogData', catalogDataRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
