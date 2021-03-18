8e16edf5104778bab61c290d70bdb9e7cd5fcd61
TreyDettmer <dettmertrey@gmail.com>
2021-03-18 10:57:28 -0700

    Merged htmlcssteam branch files with test_main branch files
    - Renamed files to have ADMIN, STAFF, or VIEWER at the start of the file
    - Removed Admin features from STAFF and VIEWER js files
    - Added images folder for catalog category images
    - Removed code containing Admin class in STAFF and VIEWER html files
    - Search.js or ADMIN_search.js is still in the catalog files. I didn't want to change those files.

---
+++ Node.js Server Workspace/public/javascripts/STAFF_individual_page.js
@@ -0,0 +1,16 @@
+// File initialized by Trey Dettmer
+
+$(document).ready(function()
+{
+
+    //allow for item to be requested
+    $("#requestButton").on("click", RequestItem)
+
+    /* Requests the item */
+    function RequestItem() {
+        //TODO: Actually send request
+        alert("Item requested.");
+    }
+
+});
+
